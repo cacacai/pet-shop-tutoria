59c8322ca05ca90b04d55b876bdbaec3ee2d6b8b
cacacai <jiashengcai97@gmail.com>
2022-04-01 02:53:31 +0800

    使用web3.js处理合约事件

--- src/js/app.js
+++ src/js/app.js
@@ -24,18 +24,39 @@ App = {
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    //https://mirror.xyz/0xABae923874F05e922A22932d8d2117ffE627212d/4irYCy6U6BpejAHDAMY_dIdzNi0l9ScNpf6WD_loysY
+    if (window.etherem) {
+      try {
+        //获取钱包授权
+        await windown.etherem.enable();
+      }catch(error) {
+        console.log(error,"获取授权失败")
+      }
+    }
+    // 传统的dapp浏览器 
+    else if (window.web3){
+      App.web3Provider = window.web3.currentProvider
+    }else {
+      //如果没有检测到注入的 web3 实例，则回退到 Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
 
+    }
+    web3 = new Web3(App.web3Provider)
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
-
+    //实例化合约
+    //载Adoption.json，保存了Adoption的ABI（接口说明）信息及部署后的网络(地址)信息，它在编译合约的时候生成ABI，在部署的时候追加网络信息
+    $.getJSON('Adoption.json',(data) => {
+      // 用Adoption.json数据创建一个可交互的TruffleContract合约实例
+      var AdoptionArtifact = data;
+      //使用 TruffleContract 进行实例化
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      //给合约注入区块链入口
+      App.contracts.Adoption.setProvider(App.web3Provider);
+      return App.markAdopted();
+    })
     return App.bindEvents();
   },
 
@@ -45,8 +66,26 @@ App = {
 
   markAdopted: function() {
     /*
-     * Replace me...
+     * 处理领养
      */
+    var adoptionInstance;
+    //在实例部署之后，的回调事件中
+    App.contracts.Adoption.deployed().then((instance) =>{
+      adoptionInstance = instance;
+      //调用合约的getAdopters()方法，用call 读取信息不用消耗gas
+      return adoptionInstance.getAdopeters().call();
+    }).then((adopters) => {
+      for (let i = 0; i < adopters.length; i++) {
+        if([adopters[i] !== '0x0000000000000000000000000000000000000000']) {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+        
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+
+    
   },
 
   handleAdopt: function(event) {
@@ -55,8 +94,26 @@ App = {
     var petId = parseInt($(event.target).data('id'));
 
     /*
-     * Replace me...
+     * 处理领养事件
      */
+    var adoptionInstance;
+    web3.eth.getAccounts((error,acounts) => {
+      if(error) {
+        console.log(error)
+      }
+      var acount = acounts[0]
+      //发起交易
+      App.contracts.Adoption.deployed().then((instance) => {
+        adoptionInstance = instance
+        //调用交易
+        return adoptionInstance.adopet(petId,{from: acount})
+      }).then((result) => {
+        //被领养，标记
+        return App.markAdopted()
+      }).catch((error) => {
+        console.log(error)
+      })
+    })
   }
 
 };
